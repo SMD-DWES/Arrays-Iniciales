12d83660e327cefcb58b6bbf89103efaa202ebc8
usasergio <usasergio2@gmail.com>
2021-09-23 14:20:41 +0200

    exp

--- diasMes.php
+++ diasMes.php
@@ -2,26 +2,10 @@
 <html>
     <head>
         <title>Pruebas array</title>
-<<<<<<< HEAD
-        <meta charset="UTF-8">
-=======
->>>>>>> b5e2b6698fd604c8986f813960955c5eae612b57
         <link href="estilo.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <?php
-<<<<<<< HEAD
-            //Me devuelve el numero de dias que tiene un mes a partir del mes pasado.
-            function decirDias($numMes) {
-                $mesDias = array(
-                    31,
-                    28,
-                    31,
-                    30,
-                    31,
-                    30,
-                    31,
-=======
         
 
             //Me devuelve el numero de dias que tiene un mes a partir del mes pasado.
@@ -34,24 +18,11 @@
                     31,//5
                     30,//6
                     31,//7
->>>>>>> b5e2b6698fd604c8986f813960955c5eae612b57
                     31,//8
                     30,
                     31,
                     30,
                     31
-<<<<<<< HEAD
-                ); // Este array establece los días MANUALMENTE.
-
-                $anio = date("Y");
-                //Si el año es bisiesto, febrero tiene 29 días, si no lo es, tiene 28.
-                if($anio % 4 == 0 && $anio %100 != 0 || ($anio %100 == 0 && $anio % 400 == 0)) 
-                    $mesDias[1] = 29;
-                else 
-                    $mesDias[1] = 28;
-
-                return $mesDias[$numMes];
-=======
                 );*/
 
                 /*$anio = date("L");
@@ -77,7 +48,6 @@
                
 
                 return $meses;
->>>>>>> b5e2b6698fd604c8986f813960955c5eae612b57
             }
 
             //Devuelve el nombre del mes a partir del número del mes.
@@ -101,13 +71,8 @@
                 return $mes[$numMes];
             }
 
-<<<<<<< HEAD
-            //Tabla con meses y días
-            echo "<table>";
-=======
             //Mejora con tablas 2
             echo "<table class='test'>";
->>>>>>> b5e2b6698fd604c8986f813960955c5eae612b57
             echo "<tr>";
 
             for($i=1;$i<=12;$i++){
@@ -115,20 +80,6 @@
             }
             echo '</tr>';
             echo '<tr>';
-<<<<<<< HEAD
-            for($i=0;$i<=11;$i++){
-                echo '<td>' . decirDias($i) . '</td>';
-            }
-            echo '</tr>';
-            echo '</table>';
-
-            //Año y dia actual.
-            echo "<h2>Año actual: ". date("Y")."</h2>";
-            echo 'Día actual: '. date("D M Y");
-
-            //Visualiza el nombre de un mes concreto.
-            echo '<br><br><br>El mes número 2 es el mes: '. devolverMes(2);
-=======
 
             foreach(decirDias() as $indice) {
                 if(isSet($indice["Febrero"][0]) == 28) 
@@ -144,7 +95,6 @@
             echo 'Día actual: '. date("D M Y");
             echo '<br>El mes numero 2 es el mes: '. devolverMes(2);
 
->>>>>>> b5e2b6698fd604c8986f813960955c5eae612b57
         ?>
     </body>
 </html>
