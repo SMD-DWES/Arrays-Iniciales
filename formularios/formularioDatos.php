829ed18f3888a443c9cbb3f75a42f239f5e60aea
2DAW04 <2DAW04@esvirgua.es>
2021-09-24 14:18:44 +0200

    Formularios

--- formularios/formularioDatos.php
+++ formularios/formularioDatos.php
@@ -1,13 +1,30 @@
 <?php
+    print_r($_GET);
+    //GET Recoge la información por URL
+    //REQUEST -> pruebas, contiene informacion de los post y gets
+    //Formularios + arrays
+
     $nombre = $_GET['nombre'];
     $correo = $_GET['email'];
-    $chxDatos = $_GET['aportarDatos'];
+
+    $chxDatos = null;
+
+    if(isset($_GET["aportarDatos"]))
+        $chxDatos = $_GET["aportarDatos"];
+
     $datosText = $_GET['datosRow'];
     $files = $_GET['files'];
 
     echo '<p>Nombre: '. $nombre.'</p>';
+    
     echo '<p>Email: '. $correo.'</p>';
     echo '<p>Aporta datos: '. $chxDatos.'</p>';
-    echo '<p>Datos: '. $datosText.'</p>';
-    echo '<p>Datos: '. $files.'</p>';
+
+    if(!empty($_GET["aportarDatos"])) {
+      echo '<p>Datos: '. $datosText.'</p>';
+    } else {
+        echo '<p>* Datos: No rellenado.</p>';
+    }
+
+    echo '<p>Fichero: '. $files.'</p>';
 ?>
